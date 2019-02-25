54ef21ce91404ec33823e26e6541a8edfd8d2b55
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-25 00:47:16 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
   # This is the array we will be passing into the remove_strawberry method
-  contacts = {
+  #contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
   #     email: "jon_snow@thewall.we",
